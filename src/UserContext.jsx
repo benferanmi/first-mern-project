447c4a7b0f929fe65d6e33fe4e5ce513a428a19e
opafunso benjamin <benferanmiopafunso@gmail.com>
2023-07-15 20:26:34 +0100

    sending the cookies from the client side to the server side

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -6,19 +6,64 @@ export const UserContext = createContext({});
 export function UserContextProvider({children}) {
     const [user,setUser] = useState(null);
     const [ready, setReady] = useState(false)
+    const [token, setToken] = useState(null)
 
-    const token = localStorage.getItem("token");
-    console.log(token)
-
-     useEffect(() =>  {
-        if(!user) {
-            axios.get('/profile').then(({data}) =>{
-                setUser(data);
-                setReady(true);
-                console.log(data)
-            })
+    const getTokenFromCookie = () => {
+        const cookies = document.cookie.split(';');
+      
+        for (let i = 0; i < cookies.length; i++) {
+          const cookie = cookies[i].trim();
+      
+          // Check if the cookie starts with "token="
+          if (cookie.startsWith('token=')) {
+            const token = cookie.split('=')[1];
+            return token;
+          }
         }
-     }, [user]);
+      
+        return null; // Token not found in cookies
+      };
+
+      useEffect(() => {
+        const recoveredToken = getTokenFromCookie();
+        setToken(recoveredToken);
+        console.log(recoveredToken);
+      }, []);
+      
+      useEffect(() => {
+        if (token && !user) {
+            const api = axios.create({
+                baseURL: 'http://localhost:4000/profile',
+              });
+          
+              api.interceptors.request.use((config) => {
+                if (token) {
+                  config.headers.Authorization = `Bearer ${token}`;
+                }
+                return config;
+              });
+
+          api.get('/').then(({ data }) => {
+            setUser(data);
+            setReady(true);
+            console.log(data);
+          })
+          .catch(error => {
+            console.error('Error fetching profile:', error);
+          });
+        }
+      }, [token, user]);
+      
+
+    //  useEffect(() =>  {
+    //     if(!user) {
+    //         axios.get('/profile').then(({data}) =>{
+    //             setUser(data);
+    //             setReady(true);
+    //             console.log(data)
+    //         })
+    //     }
+    //  }, [user]);
 
     return (
         <UserContext.Provider value={{user,setUser, ready}}>
--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -11,6 +11,7 @@ const IndexPage = () => {
       setPlaces(response.data);
     });
   }, []);
+
   return (
     <div className="mt-8 grid gap-x-6 gap-y-8 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
       {places.length > 0 &&
